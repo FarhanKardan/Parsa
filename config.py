4d7495a28d31ffc42ad5b3ed08aa64eee0592b61
farhan <58259277+FarhanKardan@users.noreply.github.com>
2025-03-15 03:02:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,38 @@
 import os
 
 # Environment variables
-BOT_TOKEN = "7805936590:AAGAea8dSNxdR2YOUTw8FmthCIuRVLYK3Y8"
-AUTHORIZED_MANAGER_IDS = [6463294729, 30209376]
+BOT_TOKEN = "7775555458:AAHSOUrl3Vt9AlAScrkwvXo_jLqC3sANixw"
+AUTHORIZED_MANAGER_IDS = [61002957,115864862]
 
 # Employee Details with IDs, Names, and Family
 EMPLOYEES = {
-    61002957: {"role": "Operations", "name": "Operations Employee 1", "family": "Family 1"},
     6463294729: {"role": "Operations", "name": "Farhan", "family": "Kardan"},
-    61002975: {"role": "Sales", "name": "Sales Employee 1", "family": "Family 2"},
-    1234567890: {"role": "Sales", "name": "Employee Name Sales", "family": "Family Sales"},
+    134124009: {"role": "Operations", "name": "Masiha", "family": "Armin"},
+    66185098: {"role": "Operations", "name": "Homa", "family": "Fadayi"},
+    210610395: {"role": "Operations", "name": "Arash", "family": "Nazeri"},
+    79266209: {"role": "Operations", "name": "Mohammad", "family": "Kiani"},
+    103244004: {"role": "Operations", "name": "Zahra", "family": "Karami"},
+    79307601: {"role": "Sales", "name": "Babak", "family": "Eftekhar"},
+    11984453885: {"role": "Sales", "name": "Ashkan", "family": "Porshir"},
+    103900682: {"role": "Sales", "name": "Masoud", "family": "Armin"},
+    318270721: {"role": "Sales", "name": "Alireza", "family": "Arab"},
+    618685441: {"role": "Operations", "name": "Nazanin", "family": "Mirzayi"},
+    5808136777: {"role": "Sales", "name": "Naser", "family": "Kiani"},
+    6118872231: {"role": "Sales", "name": "Alireza", "family": "Bakherzi"},
+
 }
 
 # Derived Groups
 OPERATIONS_EMPLOYEES = [emp_id for emp_id, details in EMPLOYEES.items() if details["role"] == "Operations"]
 SALES_EMPLOYEES = [emp_id for emp_id, details in EMPLOYEES.items() if details["role"] == "Sales"]
 
+
+
+
+# 61002957
 # Authorized Users
 AUTHORIZED_USER_IDS = list(EMPLOYEES.keys())
+
+
 
 
