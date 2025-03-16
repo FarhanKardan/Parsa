229d1286dad520620571ac0cb859d0a23336de58
root <root@srv3480498247.bitcommand.net>
2025-03-16 09:12:45 +0000

    remove some files

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 
 # Environment variables
@@ -30,8 +29,6 @@ SALES_EMPLOYEES = [emp_id for emp_id, details in EMPLOYEES.items() if details["r
 
 
 
-
-# 61002957
 # Authorized Users
 AUTHORIZED_USER_IDS = list(EMPLOYEES.keys()) + AUTHORIZED_MANAGER_IDS
 
